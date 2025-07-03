d748d0b8562cba298df02e066b619e206d29b63e
buxxket <vista_vacation661@slmail.me>
2025-07-03 22:32:22 +1000

    inital commit

---
+++ config.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-m30-sync",		1,					2},
+	{"",	"sb-video",			1,					7},
+	{"",	"sb-mic",			1,					6},
+	{"",	"sb-m30-playing",	1,					12},
+	{"",	"sb-music",			1,					11},
+	{"",	"sb-clock",			1,					1},
+	{"",	"sb-volume",		1,					10},
+	{"",	"sb-battery",		5,					3},
+	{"",	"sb-bluetooth",		5,					5},
+	{"",	"sb-internet",		5,					4},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "|";
---
+++ dwmblocks.bak/config.h
@@ -0,0 +1,15 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-video",			1,					7},
+	{"",	"sb-mic",			1,					6},
+	{"",	"sb-music",			1,					11},
+	{"",	"sb-clock",			1,					1},
+	{"",	"sb-volume",		1,					10},
+	{"",	"sb-battery",		5,					3},
+	{"",	"sb-bluetooth",		5,					5},
+	{"",	"sb-internet",		5,					4},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "|";
