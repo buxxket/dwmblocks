a760b392609ad4571d5e7c5a1f48c2e01236eb89
buxxket <vista_vacation661@slmail.me>
2025-07-21 10:40:32 +1000

    Deleted: sb-scripts/sb-battery sb-scripts/sb-bluetooth sb-scripts/sb-clock sb-scripts/sb-internet sb-scripts/sb-m30-playing sb-scripts/sb-m30-sync sb-scripts/sb-mic sb-scripts/sb-mic.bak sb-scripts/sb-music sb-scripts/sb-power sb-scripts/sb-recording sb-scripts/sb-spacer sb-scripts/sb-spotifyvol sb-scripts/sb-video sb-scripts/sb-vlcvol sb-scripts/sb-volume Modified: config.h

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",	"sb-todo",			1,					9},
 	{"",	"sb-m30-sync",		1,					2},
 	{"",	"sb-video",			1,					7},
 	{"",	"sb-mic",			1,					6},
