1551afd77b126b78eeca17e9c4518538699b2662
buxxket <vista_vacation661@slmail.me>
2025-08-19 10:30:59 +1000

    changed: command name to reflect sb-scripts change

--- config.h
+++ config.h
@@ -4,14 +4,13 @@
 #define LEADING_DELIMITER
 
 const Block blocks[] = {
-	BLOCK("sb-todo",		1,  1),
-	BLOCK("sb-m30-sync",	2,  2),
-	BLOCK("sb-video",		5,  3),
-	BLOCK("sb-mic",			5,  4),
-	BLOCK("sb-music",		0,  5),
-	BLOCK("sb-clock",		1,  6),
-	BLOCK("sb-volume",		5,  7),
-	BLOCK("sb-bluetooth",	5,	8),
-	BLOCK("sb-battery",		60,	9),
-	BLOCK("sb-internet",    5,	10),
+	BLOCK("sb-device-status",		5,  3),
+	BLOCK("sb-todo",				1,  1),
+	BLOCK("sb-m30-sync",			2,  2),
+	BLOCK("sb-music",				0,  5),
+	BLOCK("sb-clock",				1,  6),
+	BLOCK("sb-volume",				5,  7),
+	BLOCK("sb-bluetooth",			5,	8),
+	BLOCK("sb-battery",				60,	9),
+	BLOCK("sb-internet",   			5,	10),
 };
