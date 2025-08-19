203451b382182ba95916dbe37b25d462e377973d
buxxket <vista_vacation661@slmail.me>
2025-08-19 15:55:31 +1000

    changed: music refresh 0 → 1

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ const Block blocks[] = {
 	BLOCK("sb-device-status",		5,  3),
 	BLOCK("sb-todo",				1,  1),
 	BLOCK("sb-m30-sync",			2,  2),
-	BLOCK("sb-music",				0,  5),
+	BLOCK("sb-music",				1,  5),
 	BLOCK("sb-clock",				1,  6),
 	BLOCK("sb-volume",				5,  7),
 	BLOCK("sb-bluetooth",			5,	8),
